6f0f124c8e83e9a9719e7c88353ff008efab3154
Jim Rybarski <jim@rybarski.com>
2014-09-24 20:45:26 -0500

    a few tests

---
+++ tests/model/__init__.py
@@ -0,0 +1,19 @@
+import unittest
+from model import Config
+import logging
+
+
+class ConfigTests(unittest.TestCase):
+    def setUp(self):
+        self.config = Config()
+
+    def test_verbosity_default(self):
+        self.assertEqual(self.config.verbosity, logging.DEBUG)
+
+    def test_verbosity_3(self):
+        self.config.verbosity = 3
+        self.assertEqual(self.config.verbosity, logging.WARN)
+
+    def test_verbosity_2(self):
+        self.config.verbosity = 2
+        self.assertEqual(self.config.verbosity, logging.ERROR)
